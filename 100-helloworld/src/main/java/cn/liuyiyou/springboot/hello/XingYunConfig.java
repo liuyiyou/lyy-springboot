48466740692faf833a52cad40ce04b342be800e0
liuyiyou <137515110@qq.com>
2020-05-12 17:39:34 +0800

    #学习 使用redis完成分页功能，或者不使用数据库，完成增删改查

--- 100-helloworld/src/main/java/cn/liuyiyou/springboot/hello/XingYunConfig.java
+++ 100-helloworld/src/main/java/cn/liuyiyou/springboot/hello/XingYunConfig.java
@@ -2,7 +2,7 @@ package cn.liuyiyou.springboot.hello;
 
 import cn.hutool.http.HttpRequest;
 import cn.hutool.json.JSONUtil;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,14 +17,44 @@ import org.springframework.util.DigestUtils;
 @Slf4j
 public class XingYunConfig {
 
-  private static String uri = "http://openapi.xyb2b.com";
+  private static String uri = "http://120.76.191.121:81";//http://openapi.xyb2b.com";
 
-  public static String merch = "2019082160892137";
+  public static String merch = "75041628";//"2019082160892137";
 
-  public static String privateKey = "1DIPDEFCE0003900A8C00000CF4C2427";
+  public static String privateKey = "1BG847AC10007300A8C000000F634ED6";//"1DIPDEFCE0003900A8C00000CF4C2427";
 
   public static void main(String[] args) {
-    getSubsStatus();
+//    getSubsStatus();
+//    String str = "accept_name=袁莺英&address=她她她&area=重庆市&card_id=320211195505271349&city=重庆市&items=[{\"quantity\":1,\"sku_id\":\"MBS00002-B\",\"merchant_order_no\":\"zy2010000051345\"}]&merchant_id=75041628&mobile=13016313593&opcode=add_order&pay_type=1&post_code=400030&province=重庆市1BG847AC10007300A8C000000F634ED6";
+//    final String s = DigestUtils.md5DigestAsHex(str.getBytes(StandardCharsets.UTF_8));
+//    System.out.println(s.equals("93d65e4cdaa2b8f9a43dc6483784aba0"));
+    createOrder();
+  }
+
+  public static void  createOrder(){
+    String url = uri + "/api/service/business";
+    Map<String, Object> param = new HashMap<>();
+//    param.put("merchant_order_no", "zy2010000051345");
+    param.put("accept_name", "袁莺英");
+    param.put("address", "她她她");
+    param.put("area", "重庆市");
+    param.put("card_id", "320211195505271349");
+    param.put("city", "重庆市");
+    param.put("items", "[{\"merchant_order_no\":\"zy2010000051345-B\",\"quantity\":1,\"sku_id\":\"MBS00002-B\"}]");
+//    param.put("items", "[{quantity:1,sku_id:MBS00002-B,merchant_order_no:zy2010000051345}]");
+    param.put("mobile", "13016313593");
+    param.put("opcode", "add_order");
+    param.put("pay_type", "1");
+    param.put("order_no", "O15834018943291911");
+    param.put("post_code", "400030");
+    param.put("province", "重庆市");
+    param.put("merchant_id", merch);
+    param.put("sign_type", "MD5");
+    param.put("sign", sign(param));
+    System.out.println(JSONUtil.parse(param));
+    String body = HttpRequest.post(url).header("Content-Type", "application/json")
+        .body(JSONUtil.parse(param)).execute().body();
+    log.info(body);
   }
 
   public static void getSubsStatus() {
@@ -32,10 +62,9 @@ public class XingYunConfig {
     Map<String, Object> param = new HashMap<>();
     param.put("opcode", "get_order_info");
     param.put("merchant_id", merch);
-    param.put("order_no", "O35888533432492836");
+    param.put("order_no", "O15834018943291911");
     param.put("sign_type", "MD5");
     param.put("sign", sign(param));
-    System.out.println(JSONUtil.parse(param));
     String body = HttpRequest.post(url).header("Content-Type", "application/json")
         .body(JSONUtil.parse(param)).execute().body();
     log.info(body);
@@ -44,21 +73,16 @@ public class XingYunConfig {
 
   public static String sign(Map<String, Object> params) {
     TreeMap<String, Object> stringObjectTreeMap = new TreeMap<>(params);
-    String s = null;
     StringBuilder paramrBuilder = new StringBuilder();
     for (String key : stringObjectTreeMap.keySet()) {
-      if (!"sign".equals(key)  && !"sign_type".equalsIgnoreCase(key)) {
+      if (!"sign".equals(key) && !"sign_type".equalsIgnoreCase(key)) {
         paramrBuilder.append(key).append("=").append(params.get(key)).append("&");
       }
     }
-    paramrBuilder.deleteCharAt(paramrBuilder.length()-1);
+    paramrBuilder.deleteCharAt(paramrBuilder.length() - 1);
     paramrBuilder.append(privateKey);
-    System.out.println(paramrBuilder.toString());
-    try {
-      s = DigestUtils.md5DigestAsHex(paramrBuilder.toString().getBytes("UTF-8"));
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    }
-    return s;
+    String source = paramrBuilder.toString().replaceAll("\"", "");
+    System.out.println(source);
+    return DigestUtils.md5DigestAsHex(source.getBytes(StandardCharsets.UTF_8));
   }
 }
---
+++ 111-spring-cache-redis/src/test/java/cn/liuyiyou/springboot/cache/redis/RedisTemplateExample.java
@@ -0,0 +1,92 @@
+package cn.liuyiyou.springboot.cache.redis;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.sync.RedisCommands;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author: liuyiyou.cn
+ * @date: 2020/5/12
+ * @version: V1.0
+ */
+public class RedisTemplateExample {
+
+
+  //使用jedis完成功能
+  @Test
+  public void test() {
+    RedisCommands<String, String> commands = getStringStringRedisCommands();
+//    init(commands);
+    listPage(commands);
+    setPage(commands);
+
+
+    //删除一个元素
+    commands.srem("userSet","user:122");
+    commands.del("user:122");
+    commands.lrem("userList",1,"user:123");
+    commands.del("user:123");
+  }
+
+  private void init(RedisCommands<String, String> commands) {
+    for (int i = 100; i < 150; i++) {
+      commands.hset("user:" + i, "id", i + "");
+      commands.hset("user:" + i, "name", i + "lyy");
+      commands.hset("user:" + i, "email", i + "lyy.com");
+      commands.rpush("userList", "user:" + i);
+      commands.sadd("userSet", "user:" + i);
+    }
+  }
+
+  private void setPage(RedisCommands<String, String> commands) {
+    Set<String> userSet = commands.smembers("userSet");
+    System.out.println(userSet);
+    int total = userSet.size();
+    int pageSize = 10;
+    int totalPage = (int) (total / pageSize) + 1;
+    for (int i = 0; i < totalPage; i++) {
+      final List<String> userSet1 = commands.srandmember("userSet", 10);
+      for (String userKey : userSet1) {
+        Map<String, String> user = commands.hgetall(userKey);
+        System.out.print(
+            "id:" + user.get("id") + "\tname:" + user.get("name") + "\temail:" + user.get("email"));
+      }
+      System.out.println();
+    }
+  }
+
+  private void listPage(RedisCommands<String, String> commands) {
+    final Long total = commands.llen("userList");
+    int pageSize = 10;
+    int totalPage = (int) (total / pageSize) + 1;
+
+    for (int i = 0; i < totalPage; i++) {
+      System.out.println("第" + i + "页");
+      List<String> userList = commands.lrange("userList", i * pageSize, (i + 1) * pageSize);
+      for (String userKey : userList) {
+        Map<String, String> user = commands.hgetall(userKey);
+        System.out.println(
+            "id:" + user.get("id") + "\tname:" + user.get("name") + "\temail:" + user.get("email"));
+      }
+
+    }
+  }
+
+  private RedisCommands<String, String> getStringStringRedisCommands() {
+    RedisURI redisUri = RedisURI.builder()                    // <1> 创建单机连接的连接信息
+        .withHost("localhost")
+        .withPort(6379)
+        .withTimeout(Duration.of(10, ChronoUnit.SECONDS))
+        .build();
+    RedisClient redisClient = RedisClient.create(redisUri);   // <2> 创建客户端
+    return redisClient.connect().sync();
+  }
+
+
+}
