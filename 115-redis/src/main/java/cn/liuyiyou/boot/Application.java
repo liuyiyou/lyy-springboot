fdf6bd9d054c3ca0dc8a9fd2b21cf8d907dfecff
liuyiyou <137515110@qq.com>
2020-11-27 18:38:34 +0800

    Redis集成

--- 115-redis/src/main/java/cn/liuyiyou/boot/Application.java
+++ 115-redis/src/main/java/cn/liuyiyou/boot/Application.java
@@ -1,14 +1,24 @@
 package cn.liuyiyou.boot;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class Application {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+        RequestMappingHandlerMapping requestMappingHandlerMapping = run.getBean(RequestMappingHandlerMapping.class);
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
+        handlerMethods.forEach((requestMappingInfo, handlerMethod) -> {
+            requestMappingInfo.getPatternsCondition().getPatterns().forEach(System.out::println);
+        });
     }
 
 
